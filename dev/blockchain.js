051803825db35b098cd4f9be5f89cca0239d6983
roma-derski <{ID}+{username}@users.noreply.github.com>
2019-01-27 12:32:48 -0800

    hash

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,19 +1,21 @@
+const sha256 = require('sha256');
+
 function Blockchain() {
     this.chain = []; // existing blocks
-    this.newTransactions = [];
+    this.pendingTransactions = [];
 }
 
 Blockchain.prototype.createNewBlock = function(nonce, prevoiusBlockHash, hash) {
     const newBlock = {
         index: this.chain.length + 1,
         timestamp: Date.now(),
-        transactions: this.newTransactions, // put all pending txs into this new block
+        transactions: this.pendingTransactions, // put all pending txs into this new block
         nonce: nonce,
         hash: hash,
         prevoiusBlockHash: prevoiusBlockHash
     };
 
-    this.newTransactions = []; // starting over for the next block
+    this.pendingTransactions = []; // starting over for the next block
     this.chain.push(newBlock);
 
     return newBlock;
@@ -23,6 +25,23 @@ Blockchain.prototype.getLastBlock = function() {
     return this.chain[this.chain.length - 1];
 }
 
+Blockchain.prototype.createNewTransaction = function(amount, sender, recipient) {
+    const newTransaction = {
+        amount: amount,
+        sender: sender,
+        recipient: recipient
+    }
+
+    this.pendingTransactions.push(newTransaction);
+
+    return this.getLastBlock()['index'] + 1;
+}
+
+Blockchain.prototype.hashBlock = function(previousBlockHash, currentBlockData, nonce) {
+    const dataAsString = previousBlockHash + nonce.toString() + JSON.stringify(currentBlockData);
+    const hash = sha256(dataAsString);
+    return hash;
+}
 
 
 
--- dev/test.js
+++ dev/test.js
@@ -3,7 +3,31 @@ const Blockchain = require('./blockchain');
 const qacoin = new Blockchain();
 
 qacoin.createNewBlock(11, 'AAA111', 'BBB222');
-qacoin.createNewBlock(25, 'CCC333', 'DDD444');
+
+qacoin.createNewTransaction(100, 'Alice123', 'Bob456');
 qacoin.createNewBlock(81, 'EEE555', 'FFF666');
 
-console.log(qacoin.getLastBlock());
+qacoin.createNewTransaction(20, 'Bob456', 'John123');
+qacoin.createNewTransaction(30, 'Bob456', 'Randy231');
+qacoin.createNewBlock(25, 'CCC333', 'DDD444');
+
+
+const previousBlockHash = 'ASDFA234SDFAS234DFADSFA234SDFASD1234';
+const currentBlockData = [
+    {
+        amount: 10,
+        sender: 'Alice123',
+        recipient: 'Bob234'
+    },
+    {
+        amount: 20,
+        sender: 'Bob234',
+        recipient: 'Jim244'
+    }
+]
+const nonce = 100;
+
+const hash = qacoin.hashBlock(previousBlockHash, currentBlockData, nonce);
+
+//console.dir(qacoin.chain[2]);
+console.log(hash);
