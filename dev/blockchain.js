d155ad0e8adbd58fcdec67d077760d84a025ded4
roma-derski <{ID}+{username}@users.noreply.github.com>
2019-01-27 10:29:22 -0800

    createNewBlock method

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,4 +1,23 @@
 function Blockchain() {
-    this.chain = [];
+    this.chain = []; // existing blocks
     this.newTransactions = [];
 }
+
+Blockchain.prototype.createNewBlock = function(nonce, prevoiusBlockHash, Hash) {
+    const newBlock = {
+        index: this.chain.length + 1,
+        timestamp: Data.now(),
+        transactions: this.newTransactions, // put all pending txs into this new block
+        nonce = nonce,
+        hash: hash,
+        prevoiusBlockHash: prevoiusBlockHash
+    };
+
+    this.newTransactions = []; // starting over for the next block
+    this.chain.push(newBlock);
+
+    return newBlock;
+}
+
+
+
